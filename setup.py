2f274b4990a12d08d5c96bb99a679fbb8e36ad46
kirillsulim <kirillsulim@gmail.com>
2015-07-17 13:22:15 +0500

    Change version, add classifiers, add url to github repository

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='py-grabber',
-    version='0.1.1',
+    version='0.1.2',
+    url='https://github.com/kirillsulim/py-grabber',
     description='Extract main content from web-page',
     author='Kirill Sulim',
     author_email='kirillsulim@gmail.com',
@@ -17,4 +18,15 @@ setup(
             'pygrab = py_grabber.app:run',
             ],
     },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
 )
